94b5fa091cc3a23d75e308b45f84b86d830f81a8
Emily Larkin <emilylarkin@Emilys-MacBook-Air.local>
2023-10-07 22:14:13 +0100

    user input

--- hangman.py
+++ hangman.py
@@ -1,3 +1,28 @@
 import random
 from words import words
 
+def get_valid_word(words):
+    word = random.choice(words)
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()
+
+    user_letter = input('Guess a letter: ').upper()
+    if user_letter in alphabet - used_letters:
+        used_letters.add(user_letter)
+        if user_letter in word_letters:
+            word_letters.remove(user_letter)
+    
+    elif user_letter in used_letters:
+        print('You have already guessed that letter, please try again.')
+    
+    else:
+        print('Invalid character, please try again.')
+
